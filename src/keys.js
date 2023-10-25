48e30504ab76f8ee87e1ac7c21e8a5910b6a0557
Alfredo <alfredogutierrezcruzu@hotmail.com>
2023-10-25 11:54:08 -0500

    SUBIDA 25-10-2023 12:00 PM

--- src/keys.js
+++ src/keys.js
@@ -2,25 +2,26 @@ const config = require('./config');
 
 module.exports = {
 
-    /* 
+    
     database: {
       connectionLimit: 1000,
       host: config.host,
       user: config.user,
       password: config.password,
       database: config.database
-    }*/
+    }
   
 
 
-  //cuando se usa un hosting     
+  //cuando se usa un hosting 
+  /*    
   database: {
     connectionLimit: 1000,
     host: 'pro.freedb.tech',
     user: 'AdmIsmina',
     password: '$kqFCKrH8nMkXW2',
     database: 'PreIsmina'
-  }
+  }*/
 
 };
 
